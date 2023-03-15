b72a52f8f5fc8fa979b44199243cb27ced2695fa
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2023-03-15 13:04:53 +0200

    Update index.js

--- front-end-mockup/src/router/index.js
+++ front-end-mockup/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import GenderPicker from '../views/GenderPicker.vue'
 
 const routes = [
   {
